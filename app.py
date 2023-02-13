4da7429a9fade24656486ad8bab42a92538c2ecc
Yash202000 <yashpanchwatkar@gmail.com>
2023-02-13 11:58:19 +0530

    fruit detection and quality checker added

--- app.py
+++ app.py
@@ -1,92 +1,76 @@
-from flask import Flask, request, render_template
-import torch.nn as nn
-import torch
-import numpy as np
-from torchvision import transforms
+import os
+from flask import Flask, redirect, request, render_template, send_file, url_for
 import cv2
 import base64
-from net import Net
+
+
+
+from werkzeug.utils import secure_filename
+# from operations import create_table, freshness_label, get_image_by_containerid_with_tablename, imdecode_image, price_to_text, read_blob_by_id, read_blobs_from_collectiondetails, recognize_fruit_by_cv_image, write_blob
+from operations import read_blobs_from_collectiondetails ,get_image_by_containerid_with_tablename
+
+
+#  create_table,  get_image_by_containerid_with_tablename,read_blob_by_id,
+
+import os
+import sys
+import numpy as np
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
+from PIL import Image, ImageFile
+import my_tf_mod
+from io import BytesIO
+import matplotlib.pyplot as plt
+import base64
+
+
 
 app = Flask(__name__)
-ML_MODEL = None
-ML_MODEL_FILE = "model.pt"
-TORCH_DEVICE = "cpu"
-
-def get_model():
-    """Loading the ML model once and returning the ML model"""
-    global ML_MODEL
-    if not ML_MODEL:
-        ML_MODEL = Net()
-        ML_MODEL.load_state_dict(
-            torch.load(ML_MODEL_FILE, map_location=torch.device(TORCH_DEVICE))
-        )
-
-    return ML_MODEL
-
-def freshness_label(freshness_percentage):
-    if freshness_percentage > 90:
-        return "Segar"
-    elif freshness_percentage > 65:
-        return "Baik"
-    elif freshness_percentage > 50:
-        return "Cukup Baik"
-    elif freshness_percentage > 0:
-        return "Tidak Baik"
-    else:
-        return "Busuk"
-
-def price_to_text(price):
-    if price == 0:
-        return "Gratis"
-
-    return str(price)
-
-def price_by_freshness_percentage(freshness_percentage):
-    return int(freshness_percentage/100*10000)
-
-def freshness_percentage_by_cv_image(cv_image):
-    """
-    Reference: https://github.com/anshuls235/freshness-detector/blob/4cd289fb05a14d3c710813fca4d8d03987d656e5/main.py#L40
-    """
-    mean = (0.7369, 0.6360, 0.5318)
-    std = (0.3281, 0.3417, 0.3704)
-    transformation = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize(mean, std)
-    ])
-    image = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
-    image = cv2.resize(image, (32, 32))
-    image_tensor = transformation(image)
-    batch = image_tensor.unsqueeze(0)
-    out = get_model()(batch)
-    s = nn.Softmax(dim=1)
-    result = s(out)
-    return int(result[0][0].item()*100)
-
-def imdecode_image(image_file):
-    return cv2.imdecode(
-        np.frombuffer(image_file.read(), np.uint8),
-        cv2.IMREAD_UNCHANGED
-    )
-
-def recognize_fruit_by_cv_image(cv_image):
-    freshness_percentage = freshness_percentage_by_cv_image(cv_image)
-    return {
-        # TODO: change freshness_level to freshness_percentage
-        "freshness_level": freshness_percentage,
-        "price": price_by_freshness_percentage(freshness_percentage)
-    }
 
 
-## Web pages
+# create_table()
+
+
+
 
 @app.route("/")
-def index_page():
-    return render_template("index1.html")
+def home():
+    # return render_template('home.html')
+    return render_template("index.html")
+
+@app.route('/Prediction', methods=['GET','POST'])
+def pred():
+    if request.method=='POST':
+         file = request.files['file']
+         org_img, img= my_tf_mod.preprocess(file)
+
+         print(img.shape)
+         fruit_dict=my_tf_mod.classify_fruit(img)
+         rotten=my_tf_mod.check_rotten(img)
+
+         img_x=BytesIO()
+         plt.imshow(org_img/255.0)
+         plt.savefig(img_x,format='png')
+         plt.close()
+         img_x.seek(0)
+         plot_url=base64.b64encode(img_x.getvalue()).decode('utf8')
+
+
+
+    return render_template('Pred3.html', fruit_dict=fruit_dict, rotten=rotten, plot_url=plot_url)
+
+
+
 
 @app.route("/purchase", methods=["POST"])
 def purchase_page():
     return render_template("purchase.html")
+
+
+
 
 @app.route("/checkout", methods=["POST"])
 def checkout_page():
@@ -99,6 +83,7 @@ def checkout_page():
     image_content = cv2.imencode('.jpg', cv_image)[1].tobytes()
     encoded_image = base64.encodebytes(image_content)
     base64_image = 'data:image/jpg;base64, ' + str(encoded_image, 'utf-8')
+    
     return render_template(
         "checkout.html",
         freshness_percentage=freshness_percentage,
@@ -106,6 +91,7 @@ def checkout_page():
         base64_image=base64_image,
         price=price_to_text(fruit_information["price"])
     )
+
 
 
 
@@ -121,19 +107,61 @@ def login():
     return render_template('login.html')
 
 
+
+
 @app.route('/collection', methods =["GET", "POST"])
 def collection():
     if request.method=="POST":
-        print(request.form.get("cid"))
-        print(request.form.get("fid"))
-        print(request.form.get("fname"))
-        print(request.form.get("pname"))
-        print(request.form.get("pquantity"))
-        print(request.form.get("region"))
-        cv_image = imdecode_image(request.files["pimage"])
-        print(cv_image)
-        return " request handled."
+        containerid = request.form.get("cid")
+        farmerid = request.form.get("fid")
+        farmername = request.form.get("fname")
+        productname = request.form.get("pname")
+        productquantity = request.form.get("pquantity")
+        region = request.form.get("region")
+        # cv_image = imdecode_image(request.files["pimage"])
+        # print(type(cv_image))
+        f = request.files['pimage']
+        f.save(secure_filename("download.jpg"))
+        
+        # with open("download.jpg","rb") as f:
+        #     byte_data = f.read()
+        #     print(str(byte_data))
+            
+
+
+        cv_image = cv2.imread("download.jpg")
+        
+
+        fruit_information = recognize_fruit_by_cv_image(cv_image)
+        
+        freshness_percentage = fruit_information["freshness_level"]
+        image_content = cv2.imencode('.jpg', cv_image)[1].tobytes()
+        encoded_image = base64.encodebytes(image_content)
+        base64_image = 'data:image/jpg;base64, ' + str(encoded_image, 'utf-8')
+        
+        with open("download.jpg","rb") as f:
+            byte_data = f.read()
+        
+        # store data in database (if not exist container id..)
+        result = write_blob(containerid, farmerid,farmername, productname, productquantity,region,byte_data,freshness_percentage)
+        os.remove("download.jpg")
+        if result:
+            return redirect(url_for( 'collectiontable' ))
+        else:
+            return render_template("Collection.html")    
     return render_template("Collection.html")
+
+
+
+
+
+@app.route("/collectiontable",methods=['GET'])
+def collectiontable():
+    if request.method=="GET":
+        results = read_blobs_from_collectiondetails()
+        print(results)
+        
+        return render_template("CollectionTable.html",results=results)
 
 
 @app.route('/collection_handler',methods =["GET", "POST"])
@@ -146,6 +174,10 @@ def collection_handler():
     return render_template("handlercollection.html")
 
 
+
+
+
+
 @app.route('/wholesaler_handler',methods =["GET", "POST"])
 def wholesaler_handler():
     if request.method == "POST":
@@ -154,6 +186,9 @@ def wholesaler_handler():
         return render_template("admin.html")
 
     return render_template("handlerreception.html")
+
+
+
 
 @app.route('/wholesaler')
 def wholesaler():
@@ -165,6 +200,8 @@ https://youtube.com/watch?v=08S5Br_iED8&si=EnSIkaIECMiOmarE
 watch this..
 
 """
+
+
 @app.route('/treatment')
 def treatment():
     return render_template("Treatment.html")
@@ -172,8 +209,25 @@ def treatment():
 
 
 
+
 @app.route('/packaging')
 def packaging():
     return render_template("Packaging.html")
+
+
+# http://localhost:5000/send_image?tablename=collectiondetails&containerid=1
+@app.route('/send_image',methods =["GET"])
+def send_image():
+    args = request.args
+    tablename = args.get('tablename')
+    containerid = args.get('containerid')
+    if None not in (tablename, containerid):
+        blob = get_image_by_containerid_with_tablename(tablename,containerid)
+        if blob:
+            return send_file('tests/download.jpg', mimetype='image/jpg')
+        else:
+            return "None"
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
